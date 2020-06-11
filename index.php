cd236d57ac74dbda07de46b54cb06feda8105546
hishamaderis <hisham.aderis@gmail.com>
2020-06-12 05:11:58 +0800

    removed comment

--- index.php
+++ index.php
@@ -11,8 +11,7 @@ table, th, td {
 
 <?php>
 echo "<table><tr><th>ID</th><th>Name</th></tr>";
-  // output data of each row
-    echo "<tr><td>""</td><td>""</td></tr>";
+echo "<tr><td>""</td><td>""</td></tr>";
   }
   echo "</table>";
 <?>
