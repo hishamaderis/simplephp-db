2218f76c07c581fd9ccf4faa584c3f8d80b0f8fd
hishamaderis <hisham.aderis@gmail.com>
2020-06-12 05:21:49 +0800

    html table testing

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!--
 <!DOCTYPE html>
 <html>
     <h1> This is SPARTA</h1>
@@ -19,3 +20,38 @@ echo "<tr><td>""</td><td>""</td></tr>";
 
 </body>
 </html>
+-->
+    
+<html>
+<body>
+
+<h2>Basic HTML Table</h2>
+
+<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+  <tr>
+    <td>John</td>
+    <td>Doe</td>
+    <td>80</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
+    
+
